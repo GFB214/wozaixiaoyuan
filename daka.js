6c4b9c385b6c393d496a3660751f44c8fe5b3e63
Milky01 <50903087+Milky01@users.noreply.github.com>
2021-07-07 22:36:23 +0800

    Update and rename hello.js to daka.js

--- daka.js
+++ daka.js
@@ -3,16 +3,16 @@ const KEY = 'token'
 function GetSeq(){
   const myDate = new Date()
   const hours = myDate.getHours()
-  if(0<=hours && hours<=9){
+  if(0<=hours && hours<=8){
     return 1
   }
-  if(11<=hours && hours<=15){
+  if(11<=hours && hours<=14){
     return 2
   }
-  if(17<=hours && hours<=21){
+  if(17<=hours && hours<=22){
     return 3
   }
-  return 0
+  return -1
 }
 
 function Sign(){
@@ -27,7 +27,13 @@ function Sign(){
   'User-Agent' : `Mozilla/5.0 (iPhone; CPU iPhone OS 14_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 MicroMessenger/7.0.18(0x17001229) NetType/4G Language/zh_CN`,
   'token' : $prefs.valueForKey(KEY)
   };
-  const body = `answers=%5B%220%22%5D&seq=`+ GetSeq() +`&temperature=36.0&userId=&latitude=23.084003&longitude=113.317412&country=%E4%B8%AD%E5%9B%BD&city=%E5%B9%BF%E5%B7%9E%E5%B8%82&district=%E6%B5%B7%E7%8F%A0%E5%8C%BA&province=%E5%B9%BF%E4%B8%9C%E7%9C%81&township=%E6%B1%9F%E6%B5%B7%E8%A1%97%E9%81%93&street=%E4%B8%8A%E5%86%B2%E4%B8%AD%E7%BA%A6%E6%96%B0%E8%A1%97%E4%B8%80%E5%B7%B7&myArea=`;
+  
+  seq = GetSeq()
+  if (req == -1) {
+    $notify("打卡失败","不在打卡时段")
+    return
+  }
+  const body = `answers=%5B%220%22%5D&seq=`+ seq +`&temperature=36.0&userId=&latitude=23.084003&longitude=113.317412&country=%E4%B8%AD%E5%9B%BD&city=%E5%B9%BF%E5%B7%9E%E5%B8%82&district=%E6%B5%B7%E7%8F%A0%E5%8C%BA&province=%E5%B9%BF%E4%B8%9C%E7%9C%81&township=%E6%B1%9F%E6%B5%B7%E8%A1%97%E9%81%93&street=%E4%B8%8A%E5%86%B2%E4%B8%AD%E7%BA%A6%E6%96%B0%E8%A1%97%E4%B8%80%E5%B7%B7&myArea=`;
   
   const myRequest = {
       url: url,
@@ -61,7 +67,6 @@ function ReadToken(){
  console.log($prefs.valueForKey(KEY))
  Sign()
 }
-
 
 ReadToken()
 $done({})
